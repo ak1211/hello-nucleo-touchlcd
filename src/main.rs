abf41cbcd23205431efe50f7439e8253ad2f1a48
ak1211 <ak1211@mail.ak1211.com>
2020-03-01 21:57:41 +0900

    first commit

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ extern crate embedded_hal;
 extern crate stm32f30x_hal as hal;
 
 extern crate embedded_graphics;
-extern crate ssd1331;
+extern crate ili9341;
 
 use hal::delay::Delay;
 use hal::gpio::gpioa::PA9;
@@ -24,12 +24,17 @@ use hal::spi::Spi;
 use hal::stm32f30x;
 
 use cortex_m_rt::entry;
-use embedded_graphics::{
-    coord::Coord, fonts::Font6x8, pixelcolor::PixelColorU16, prelude::*, primitives::Circle,
-    primitives::Line, primitives::Rect, primitives::Triangle,
-};
-use embedded_hal::digital::v2::OutputPin;
-use ssd1331::{prelude::*, Builder};
+
+use embedded_graphics::fonts::Font12x16;
+use embedded_graphics::fonts::Font6x8;
+use embedded_graphics::fonts::Font8x16;
+use embedded_graphics::geometry::Point;
+use embedded_graphics::pixelcolor::RgbColor;
+use embedded_graphics::prelude::*;
+use embedded_graphics::primitives::{Circle, Line, Rectangle, Triangle};
+
+use ili9341::spi::SpiInterface;
+use ili9341::*;
 
 #[entry]
 fn main() -> ! {
@@ -53,7 +58,7 @@ fn main() -> ! {
     let mut gpiob = p.GPIOB.split(&mut rcc.ahb);
     let mut gpioc = p.GPIOC.split(&mut rcc.ahb);
 
-    // OLED display SPI interface
+    // LCD SPI interface
     let sck = gpiob.pb13.into_af5(&mut gpiob.moder, &mut gpiob.afrh);
     let miso = gpiob.pb14.into_af5(&mut gpiob.moder, &mut gpiob.afrh);
     let mosi = gpiob.pb15.into_af5(&mut gpiob.moder, &mut gpiob.afrh);
@@ -64,88 +69,129 @@ fn main() -> ! {
             polarity: embedded_hal::spi::Polarity::IdleLow,
             phase: embedded_hal::spi::Phase::CaptureOnFirstTransition,
         },
-        8.mhz(),
+        32.mhz(),
         clocks,
         &mut rcc.apb1,
     );
 
-    // OLED display CS, D/C, RESET pins
-    let mut oled_cs: PB6<Output<PushPull>> = gpiob
+    // LCD CS, D/C, RESET pins
+    let lcd_cs: PB6<Output<PushPull>> = gpiob
         .pb6
         .into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
 
-    let oled_dc: PA9<Output<PushPull>> = gpioa
+    let lcd_dc: PA9<Output<PushPull>> = gpioa
         .pa9
         .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
 
-    let mut oled_reset: PC7<Output<PushPull>> = gpioc
+    let lcd_reset: PC7<Output<PushPull>> = gpioc
         .pc7
         .into_push_pull_output(&mut gpioc.moder, &mut gpioc.otyper);
 
-    let mut display: GraphicsMode<_> = Builder::new().connect_spi(spi, oled_dc).into();
+    let iface = SpiInterface::new(spi, lcd_cs, lcd_dc);
+    let mut display = Ili9341::new(iface, lcd_reset, &mut delay).unwrap();
 
-    // fall down to low the CS pin, activate the OLED display
-    OutputPin::set_low(&mut oled_cs).unwrap();
-
-    // initialize
-    display.reset(&mut oled_reset, &mut delay);
-    display.init().unwrap();
-
-    let color = PixelColorU16(0xffff_u16);
-
+    let w = display.width() as i32;
+    let h = display.height() as i32;
     display.draw(
-        Triangle::new(Coord::new(4, 20), Coord::new(12, 6), Coord::new(20, 20))
-            .with_stroke(Some(color))
-            .with_stroke_width(3)
-            .into_iter(),
+        Rectangle::new(Point::new(0, 0), Point::new(w - 1, h - 1)).fill(Some(RgbColor::BLACK)),
     );
 
     display.draw(
-        Circle::new(Coord::new(30, 13), 7)
-            .with_stroke(Some(color))
-            .with_stroke_width(1)
-            .into_iter(),
+        Triangle::new(Point::new(45, 5), Point::new(5, 75), Point::new(85, 75))
+            .stroke(Some(RgbColor::RED))
+            .fill(Some(RgbColor::RED))
+            .stroke_width(3),
     );
 
     display.draw(
-        Rect::new(Coord::new(42, 6), Coord::new(42 + 14, 6 + 14))
-            .with_stroke(Some(color))
-            .with_stroke_width(1)
-            .into_iter(),
+        Circle::new(Point::new(120, 37), 36)
+            .stroke(Some(RgbColor::GREEN))
+            .fill(Some(RgbColor::GREEN)),
     );
 
     display.draw(
-        Line::new(Coord::new(5, 25), Coord::new(60, 25))
-            .with_stroke(Some(color))
-            .with_stroke_width(10)
-            .into_iter(),
+        Rectangle::new(Point::new(165, 5), Point::new(165 + 69, 5 + 69))
+            .stroke(Some(RgbColor::BLUE))
+            .fill(Some(RgbColor::BLUE)),
+    );
+
+    display.draw(
+        Line::new(Point::new(5, 100), Point::new(100, 100))
+            .stroke(Some(RgbColor::GREEN))
+            .stroke_width(20),
     );
 
     display.draw(
         Font6x8::render_str("Hello World!")
-            .with_stroke(Some(PixelColorU16(0xf800_u16)))
-            .with_stroke_width(2)
-            .translate(Coord::new(5, 30))
+            .stroke(Some(RgbColor::RED))
+            .stroke_width(2)
+            .translate(Point::new(5, 110))
             .into_iter(),
     );
 
     display.draw(
         Font6x8::render_str("Hello Rust!")
-            .with_stroke(Some(PixelColorU16(0x07e0_u16)))
-            .with_stroke_width(2)
-            .translate(Coord::new(5, 40))
+            .stroke(Some(RgbColor::GREEN))
+            .stroke_width(2)
+            .translate(Point::new(5, 120))
             .into_iter(),
     );
 
     display.draw(
         Font6x8::render_str("Hello stm32!")
-            .with_stroke(Some(PixelColorU16(0x001f_u16)))
-            .with_stroke_width(2)
-            .translate(Coord::new(5, 50))
+            .stroke(Some(RgbColor::BLUE))
+            .stroke_width(2)
+            .translate(Point::new(5, 130))
             .into_iter(),
     );
 
-    display.flush().unwrap();
+    display.draw(
+        Font8x16::render_str("Hello World!")
+            .stroke(Some(RgbColor::RED))
+            .stroke_width(2)
+            .translate(Point::new(5, 150))
+            .into_iter(),
+    );
+
+    display.draw(
+        Font8x16::render_str("Hello Rust!")
+            .stroke(Some(RgbColor::GREEN))
+            .stroke_width(2)
+            .translate(Point::new(5, 170))
+            .into_iter(),
+    );
+
+    display.draw(
+        Font8x16::render_str("Hello stm32!")
+            .stroke(Some(RgbColor::BLUE))
+            .stroke_width(2)
+            .translate(Point::new(5, 190))
+            .into_iter(),
+    );
+
+    display.draw(
+        Font12x16::render_str("Hello World!")
+            .stroke(Some(RgbColor::RED))
+            .stroke_width(2)
+            .translate(Point::new(5, 220))
+            .into_iter(),
+    );
+
+    display.draw(
+        Font12x16::render_str("Hello Rust!")
+            .stroke(Some(RgbColor::GREEN))
+            .stroke_width(2)
+            .translate(Point::new(5, 250))
+            .into_iter(),
+    );
+
+    display.draw(
+        Font12x16::render_str("Hello stm32!")
+            .stroke(Some(RgbColor::BLUE))
+            .stroke_width(2)
+            .translate(Point::new(5, 280))
+            .into_iter(),
+    );
 
     loop {}
 }
